f44b7e32ce7ef9cfa1a64297ac48350ad80f6251
Tobias Gurdan <tobias.gurdan@intel.com>
2018-11-22 17:06:08 +0100

    Set theme default colors

--- include/MessageCenter/theme.h
+++ include/MessageCenter/theme.h
@@ -23,13 +23,22 @@ class Theme
         void setColor( const std::string& key, const uint8_t primary );
         void setColor( const std::string& key, const uint8_t primary, const uint8_t secondary );
 
-        const IndexPair& colors( const std::string& key ) const;
-        const IndexPair& textColors() const { return textColors_; }
+        const IndexPair& indices( const std::string& key ) const;
+        const IndexPair& primaryIds() const { return primaryIds_; }
+        const IndexPair& secondaryIds() const { return secondaryIds_; }
 
     private:
 
-        std::unordered_map<std::string, IndexPair> colors_;
-        IndexPair textColors_ = { 251, 246 };
+        std::unordered_map<std::string, IndexPair> colors_ = {
+            { "info", { 39, 45 } },
+            { "success", { 40, 46 } },
+            { "warning", { 220, 226 } },
+            { "error", { 196, 202 } },
+            { "debug", { 159, 195 } }
+        };
+        
+        IndexPair primaryIds_ = { 254, 246 };
+        IndexPair secondaryIds_ = { 242, 238 };
 };
 
 
--- src/theme.cpp
+++ src/theme.cpp
@@ -5,22 +5,15 @@ namespace mc {
 
 
 ////////////////////////////////////////////////////////////////////////////////
-Theme::Theme() :
-    colors_({
-        { "info", { 39, 45 } },
-        { "success", { 40, 46 } },
-        { "warning", { 220, 226 } },
-        { "error", { 196, 202 } },
-        { "debug", { 159, 195 } }
-    })
+Theme::Theme()
 {}
 
 
 ////////////////////////////////////////////////////////////////////////////////
-const IndexPair& Theme::colors( const std::string& key ) const
+const IndexPair& Theme::indices( const std::string& key ) const
 {
     if ( colors_.find( key ) == colors_.end() ) {
-        return textColors();
+        return primaryIds();
     }
 
     return colors_.at( key );
