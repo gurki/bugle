d0fddcae6ca5b19066788de9505f6df57a7b143b
Tobias Gurdan <tobias.gurdan@intel.com>
2018-11-27 13:29:59 +0100

    Make message construction non-blocking for other postAsync calls

--- src/messagecenter.cpp
+++ src/messagecenter.cpp
@@ -53,13 +53,13 @@ void MessageCenter::post(
 
     std::cout << "  " << DateTime::now().timeInfo() << " -- enter post " << std::this_thread::get_id() << std::endl;
 
-    //  thread::detach instead of async, to allow non-blocking continuation from caller thread
-    //  arguments are copied to ensure thread-safety
+    //  asynchronous construction of non-moveable Message in case of time-consuming tag parsing.
+    //  use thread::detach instead of async, to allow non-blocking continuation from caller thread.
+    //  arguments are copied to ensure thread-safety.
     //
     //  TODO(tgurdan): 
     //    provide rvalue reference implementation to avoid copies if not necessary
     std::thread( &MessageCenter::postAsync, this, content, MC_INFO_NAMES, tags ).detach();
-    // std::future<void> f = std::async( std::launch::async, &MessageCenter::postAsync, this, content, MC_INFO_NAMES, tags );
     
     std::cout << "  " << DateTime::now().timeInfo() << " -- exit post " << std::this_thread::get_id() << std::endl;
 }
@@ -71,19 +71,17 @@ void MessageCenter::postAsync(
     MC_INFO_DECLARE,
     const nlohmann::json& tags )
 {
+    using namespace std::chrono_literals;
+    std::this_thread::sleep_for( std::chrono::milliseconds( std::rand() % 500 ) );
+    std::cout << "    " << DateTime::now().timeInfo() << " -- enter postAsync " << std::this_thread::get_id() << std::endl;
+    const auto message = Message( MC_INFO_NAMES, content, tags );
+
     //  avoid observer insertion during iteration
     //  ensure multiple postAsync calls run sequentially to keep chronological order
     std::lock_guard<std::mutex> guard( observerMutex_ );
-    
-    using namespace std::chrono_literals;
-    std::this_thread::sleep_for( 1s );
 
-    std::cout << "    " << DateTime::now().timeInfo() << " -- enter postAsync " << std::this_thread::get_id() << std::endl;
     std::cout << "    " << content << std::endl;
     std::this_thread::sleep_for( 2s );
-
-    const auto message = Message( MC_INFO_NAMES, content, tags );
-
 
     for ( const auto& observerRef : observers_ )
     {
