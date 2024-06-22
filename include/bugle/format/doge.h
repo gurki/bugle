22da7582d96546b98bb49bb4f5f53ecd1ea4617f
Tobias Gurdan <tobias@gurdan.de>
2024-06-22 02:30:56 +0200

    Start porting logger confirmations

---
+++ include/bugle/format/doge.h
@@ -0,0 +1,88 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+namespace bugle {
+
+
+const std::vector<std::string> firstWords = {
+    "WOW",
+    "SUCH",
+    "MANY",
+    "GREAT",
+    "AMAZING",
+    "FANTASTIC",
+    "AWESOME",
+    "INCREDIBLE",
+    "IMPRESSIVE",
+    "EXCELLENT",
+    "SUPERB",
+    "PHENOMENAL",
+    "OUTSTANDING",
+    "BRILLIANT",
+    "SUPER",
+    "FABULOUS",
+    "TERRIFIC",
+    "SPECTACULAR",
+    "WONDERFUL",
+    "STUNNING"
+};
+
+const std::vector<std::string> secondWords = {
+    "SUCCESS",
+    "EFFORT",
+    "GAIN",
+    "VICTORY",
+    "ACHIEVE",
+    "HAPPY",
+    "TRIUMPH",
+    "ACCOMPLISHMENT",
+    "WIN",
+    "EXECUTION",
+    "RUNTIME",
+    "COMPILE",
+    "IMPROVE",
+    "STABILITY",
+    "FEATURES",
+    "RESULT",
+    "WORK",
+    "CODE",
+    "FUNCTION",
+    "RESPONSE",
+    "CLEAN",
+    "DEBUG",
+    "OPTIMIZE",
+    "SECURITY",
+    "FLEXIBILITY",
+    "UPDATES",
+    "FIX",
+    "MAINTENANCE",
+    "SIMPLICITY",
+    "PROGRESS",
+    "LOGIC",
+    "DESIGN",
+    "INTEGRATION",
+    "INNOVATION",
+    "COMPATIBILITY",
+    "MODULARITY",
+    "ENHANCE",
+    "DYNAMICS",
+    "SOPHISTICATION",
+    "FUNCTIONALITY",
+    "INTERFACE",
+    "ARTISTRY",
+    "POTENTIAL",
+    "ADAPTATION",
+    "CUSTOMIZATION",
+    "POWER",
+    "VERSATILITY",
+    "USABILITY",
+    "SMOOTHNESS",
+    "DEPENDABILITY",
+    "SOLUTIONS",
+    "PERFORMANCE"
+};
+
+
+}   //  ::bugle
