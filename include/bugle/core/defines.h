7cf92f59ada12160d1def433cde80fb9b0fbc5b4
Tobias Gurdan <tobias@novaskystories.com>
2024-06-25 18:59:43 +0200

    Fix empty PO and parent scope define

--- include/bugle/core/defines.h
+++ include/bugle/core/defines.h
@@ -1,12 +1,12 @@
 #pragma once
 
+#define PO bugle::PostOffice::instance()
+
 #ifdef BUGLE_ENABLE
-    #define PO bugle::PostOffice::instance()
     #define POST( letter, tags, attributes ) bugle::PostOffice::instance().post( BUGLE_INFO_VALUES, letter, tags, attributes )
     #define ENVELOPE( title ) bugle::Envelope __envelope( bugle::PostOffice::instance(), title )
     #define ENVELOPE_CLOSE() __envelope.close()
 #else
-    #define PO
     #define POST( letter, tags, attributes )
     #define ENVELOPE( title )
     #define ENVELOPE_CLOSE()
